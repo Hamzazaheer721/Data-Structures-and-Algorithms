2834bac371d5ecf3e7e5489c36a83d6d022748f9
Hamza Zaheer <62650512+Hamzazaheer721@users.noreply.github.com>
2022-01-08 00:05:31 +0500

    !feat: frequeny pattern is being understood

--- 3_Problem_Solving_Approach/index.ts
+++ 3_Problem_Solving_Approach/index.ts
@@ -1,3 +1,4 @@
+/* Naive solutions */
 const sameFunc = (arrOne: number[], arrTwo: number[]): boolean => {
   let res: boolean = false
   if (arrOne.length !== arrTwo.length) return res
@@ -26,7 +27,18 @@ const sameFunc_ = (arrOne: number[], arrTwo: number[]): boolean => {
   return res
 }
 
+const sameFuncConcise = (arrOne: number[], arrTwo: number[]): boolean => {
+  if (arrOne.length !== arrTwo.length) return false
+  for (let i = 0; i < arrOne.length; i++) {
+    let arr_index = arrTwo.indexOf(arrOne[i] ** 2)
+    if (arr_index === -1) return false
+    arrTwo.splice(arr_index, 1) //shorten the array with every successful result
+  }
+  return true
+}
+
 array1 = [1, 2, 3, 5]
 array2 = [9, 1, 25, 4]
 
 console.log(`Result: ${sameFunc_(array1, array2)}`)
+console.log(`Result: ${sameFuncConcise(array1, array2)}`)
