8aa0527483a7bab1b8ddc148f3a48f5360798205
Hamza Zaheer <62650512+Hamzazaheer721@users.noreply.github.com>
2022-01-08 02:29:23 +0500

    !feat(bubble sort): shortand for swapping added

--- 6_Sorting/Bubble Sort/1_bubbleSort.ts
+++ 6_Sorting/Bubble Sort/1_bubbleSort.ts
@@ -2,9 +2,7 @@ const bubbleSortAsc = (arr: number[]): number[] => {
   for (let i = 0; i < arr.length; i++) {
     for (let j = 0; j < arr.length; j++) {
       if (arr[j] > arr[j + 1]) {
-        let val = arr[j]
-        arr[j] = arr[j + 1]
-        arr[j + 1] = val
+        ;[arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]
       }
     }
   }
